77d2e78235463b7fd32d504f9de629cc12ee44aa
Ivo Stratev <ivo.stratev.tues@gmail.com>
2016-12-04 11:44:25 +0200

    Create argv_example.cpp

---
+++ argv_example.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+void print(std::string message) {
+    std::cout << message << std::endl;
+}
+
+void flag(char flag) {
+    switch(flag) {
+        case 'm':
+            print("message flag");
+            break;
+        case 'A':
+            print("All");
+            break;
+        case 'f':
+            print("force");
+            break;
+        default:  print("Unknown flag");
+    }
+}
+
+int main(int argc, char**argv) {
+    bool arg = false;
+    for(int i = 0; i < argc; ++i) {
+        if(argv[i][0] == '-') {
+            arg = true;
+            print("Flag:");
+            flag(argv[i][1]);
+        } else {
+            print(arg ? "argument" : "Unknown");
+            print(std::string(argv[i]));
+        }
+    }
+
+    return 0;
+}
